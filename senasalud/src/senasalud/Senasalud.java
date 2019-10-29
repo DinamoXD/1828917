a0c91b7821b52acdbf70663609b0c2f380d04427
Christian <sala@HOR-SALA501-013.unihorizonte.salas>
2019-10-29 16:48:41 -0500

    Mi segundo commit

--- senasalud/src/senasalud/Senasalud.java
+++ senasalud/src/senasalud/Senasalud.java
@@ -8,10 +8,31 @@ public class Senasalud {
   
     public static void main(String[] args) {
       String nom;
+      
         Scanner nombre =new Scanner (System.in); 
         System.out.println("Hola bienvenido, ingrese su nombre");
         nom=nombre.next();
+        
+        String ape;
+        Scanner apellido = new Scanner (System.in);
+        System.out.println("Digite el apellido");
+        ape=apellido.next();
+        
+        
+        int eda;        
+        Scanner edad = new Scanner (System.in);
+        System.out.println("Ingresa la edad");
+        eda=edad.nextInt();
+        
+       
+        
+        
         System.out.println("su nombre es:" + nom);
+        System.out.println("Su apellido es:" + eda);
+        System.out.println("Su edad es:" + ape);
+      
     }
+    
+    
     
 }
